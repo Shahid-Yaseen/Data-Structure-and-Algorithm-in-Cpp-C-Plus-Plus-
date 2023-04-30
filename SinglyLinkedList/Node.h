3362e6caf4c14ba2b08378b3e7983a32f9f43c3c
Shahid-Yaseen <129145174+Shahid-Yaseen@users.noreply.github.com>
2023-04-30 09:15:54 -0700

    Add files via upload

---
+++ SinglyLinkedList/Node.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+struct Node{
+    //next Node will point to the next of present Node
+    Node* next;
+    //data will store the value of present Node
+    int data;
+};
